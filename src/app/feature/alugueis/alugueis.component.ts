b9cf1b9cf542dfe5aba1850168ebd05725bfb6a5
Cleverson Parreira Júnior <parreirajr@gmail.com>
2025-05-11 18:08:28 -0300

    Criação de componentes de aluguéis e relatórios
        - Criação de alugueis.component.html, alugueis.component.scss, alugueis.component.ts: componente básico para aluguéis.
        - Criação de relatorios.component.html, relatorios.component.scss, relatorios.component.ts: componente básico para relatórios.
        - Motivo: Estabelecer base para funcionalidades de aluguéis e relatórios.

---
+++ src/app/feature/alugueis/alugueis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alugueis',
+  standalone: true,
+  imports: [],
+  templateUrl: './alugueis.component.html',
+  styleUrl: './alugueis.component.scss'
+})
+export class AlugueisComponent {
+
+}
