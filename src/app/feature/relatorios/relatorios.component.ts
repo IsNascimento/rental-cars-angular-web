a4031b98df38a74a5e85ed8a5f88e2fd32cf64a5
Cleverson Parreira Júnior <parreirajr@gmail.com>
2025-05-12 04:26:33 -0300

    Implementação de relatórios de aluguéis
        - Atualização em relatorios.component.html: adicionados filtros (data e modelo) e tabela de aluguéis.
        - Criação de estilos em relatorios.component.scss: estilização de filtros, tabela e tags.
        - Modificação em relatorios.component.ts: implementada lógica de busca e exibição de relatórios.
        - Motivo: Habilitar consulta de aluguéis com filtros e soma de débitos.

--- src/app/feature/relatorios/relatorios.component.ts
+++ src/app/feature/relatorios/relatorios.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { AlugueisService, Aluguel } from './service/relatorios.service';
+import { CalendarModule } from 'primeng/calendar';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt'
+import { TitleContentComponent } from "../../shared/components/title-content/title-content.component";
+
+registerLocaleData(localePt)
 
 @Component({
   selector: 'app-relatorios',
   standalone: true,
-  imports: [],
+  imports: [
+    CalendarModule,
+    CommonModule,
+    DividerModule,
+    DropdownModule,
+    FormsModule,
+    TableModule,
+    TagModule,
+    TitleContentComponent
+],
   templateUrl: './relatorios.component.html',
-  styleUrl: './relatorios.component.scss'
+  styleUrl: './relatorios.component.scss',
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }]
 })
-export class RelatoriosComponent {
+export class RelatoriosComponent implements OnInit {
+  dataAluguel!: Date;
+  modeloCarros: { label: string; value: string | null }[] = [
+    { label: 'Todos', value: null }
+  ];
+  modeloCarroSelecionado: string | null = null;
 
+  alugueis: Aluguel[] = [];
+  valornaoPago: number = 0;
+
+  columns = [
+    { field: 'dataAluguel', header: 'DATA' },
+    { field: 'modeloCarro', header: 'MODELO' },
+    { field: 'km', header: 'KM' },
+    { field: 'nomeCliente', header: 'CLIENTE' },
+    { field: 'telefone', header: 'TELEFONE' },
+    { field: 'dataDevolucao', header: 'DEVOLUÇÃO' },
+    { field: 'pago', header: 'PAGO' },
+    { field: 'valor', header: 'VALOR' }
+  ];
+
+  constructor(private alugueisService: AlugueisService) { }
+
+  ngOnInit(): void {
+    this.alugueisService.getRelatorio().subscribe((dados) => {
+      this.alugueis = dados.alugueis;
+      this.valornaoPago = dados.valorNaoPago;
+      this.modeloCarros = [
+        { label: 'Todos', value: null }
+      ];
+
+      const modelosSet = new Set<string>();
+      dados.alugueis.forEach(aluguel => {
+        modelosSet.add(aluguel.modeloCarro);
+      });
+
+      modelosSet.forEach(modelo => {
+        this.modeloCarros.push({ label: modelo, value: modelo })
+      });
+    });
+  }
+
+  onSearch() {
+    const isoDate = this.dataAluguel instanceof Date ? this.dataAluguel.toISOString().split('T')[0] : this.dataAluguel;
+
+    this.alugueisService.getRelatorio(isoDate, this.modeloCarroSelecionado)
+      .subscribe((dados) => {
+        this.alugueis = dados.alugueis;
+        this.valornaoPago = dados.valorNaoPago;
+      })
+  }
 }
